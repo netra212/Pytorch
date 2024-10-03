9a0c86ac05deff346c5ecb6cfb1013673f354304
netra2127 <netra200021kcbdr@gmail.com>
2024-10-03 13:07:23 +0100

    Linear Regression Modules with PyTorch

---
+++ 02_PyTorch_Workflow/00_LR_Model.py
@@ -0,0 +1,25 @@
+import torch 
+from torch import nn
+
+# Create a Linear Regression model in PyTorch.
+class LinearRegressionModule(nn.Module):
+    '''Subclass -> nn.Module (this contains all the building bocks for neural networks)'''
+    def __init__(self):
+        super.__init__()
+
+        '''Initialize model parameters to be used in various computations (these could be different layers from torch.nn, single parameters, hard-coded values or functions).'''
+        # Initialize Model parameters. 
+        self.weights = nn.Parameter(torch.randn(1,
+                                                requires_grad=True, 
+                                                dtype=torch.float))
+        '''requires_grad = True means PyTorch will track the gradients of this specific parameter for use with torch.autograd (torch.autograd <- implements gradient descent) and gradient descent (for many torch.nn modules, requires_grad=True is set by default)'''
+        self.bias = nn.Parameter(torch.randn(1, 
+                                             requires_grad=True,
+                                             dtype=torch.float))
+    
+    # forward() defines the computation in the model. 
+    def forward(self, x:torch.Tensor) -> torch.Tensor:
+        '''Any subclass of nn.Module needs to overwrite forward() (this defines the forward computation of the model). In this case, forward() method implements the LinearRegressions Module.'''
+        return self.weights * x + self.bias
+    
+
